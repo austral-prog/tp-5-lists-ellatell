d94a82328d717d9cfd97ebb06c9c2e8e5e72dd99
ellatell <etell@mail.austral.edu.ar>
2026-04-28 10:25:20 -0300

    Update exercise_find_min.py

--- exercise_find_min.py
+++ exercise_find_min.py
@@ -1,17 +1,15 @@
 # Ejercicio 6: Encontrar el mínimo en una lista
-
-def find_max(lista):
+def find_min(lista):
     """
-    Encuentra y retorna el valor máximo en una lista de números.
+    Encuentra y retorna el valor mínimo en una lista de números.
     Si la lista está vacía, retorna None.
 
     Args:
         lista: Una lista de números
 
     Returns:
-        El valor máximo de la lista o None si está vacía
+        El valor mínimo de la lista o None si está vacía
     """
     if len(lista) == 0:
         return None
-
-    return max(lista)
+    return min(lista)
